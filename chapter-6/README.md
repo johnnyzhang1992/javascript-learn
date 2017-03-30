fe7f91afff0ebc34fa269374cd15ca3956c1bc63
johnnyzhang <1113638674@qq.com>
2017-03-30 16:33:50 +0800

    chapter-6 JavaScript 与 CSS

---
+++ chapter-6/README.md
@@ -0,0 +1,29 @@
+## JavaScript 与 CSS
+
+### 访问样式信息
+
+访问个设置CSS 属性的主要工具是元素自身的样式属性。
+比如：``` elem.style.helght = 100px  ```.
+
+### 动态元素
+
+创建动态效果有3个至关重要的属性： 位置、尺寸和可见性。
+
+1. 元素的位置（position）
+
+```
+ - 静态定位：static 
+ - 相对定位：relative
+ - 绝对定位：absolute
+ - 固定定位：fixed
+```
+ * 获取位置
+ 
+ ```
+ offsetParent: 理论上，这是元素的父亲，元素相对于它定位。实际上，所指向的元素取决于浏览器 
+ 
+ offsetLeft和offsetTop: 这两个属性分别是元素在offsetParent 上下文中的水平和垂直偏移量。
+ ```
+ * 设置位置
+ 
+ 2. 元素的尺寸
---
+++ chapter-6/style.js
@@ -0,0 +1,205 @@
+/**
+ * Created by zq199 on 2017/3/30.
+ */
+
+//----- 获取元素的真实、最终的CSS 样式属性值的函数-----------
+// 获取指定元素(elem) 的样式属性（name）
+function getStyle(elem ,name) {
+    // 如果属性存在于style[]中，那么它已被设置了(并且是当前的)
+    if( elem.style[name]){
+        console.log("Normal");
+        return elem.style[name];
+    }else if(elem.currentStyle){
+        // 否则，尝试使用IE的方法
+        console.log("IE");
+        return elem.currentStyle[name];
+    }else if( document.defaultView &&　document.defaultView.getComputedStyle){
+        // 或者W3C 的方法，如果存在的话
+
+        // 它使用的是通用的 ' text-align' 的样式规则而非 'textAlign'
+        name = name.replace(/([A-Z])/g,"-$1");
+        name = name.toLowerCase();
+
+        // 获取样式对象并获取属性（存在的话）值
+        var s = document.defaultView.getComputedStyle(elem,"");
+        console.log("W3C");
+        return s && s.getPropertyValue(name);
+    }else{
+        // 否则，用户使用的是其他浏览器
+        console.log("Other");
+        return null;
+    }
+
+}
+
+/**  ------------- 动态元素 ---------- */
+
+/* -----获取位置------------- */
+
+//----两个确定元素相对于整个文档的x和y位置的辅助函数------
+// 获取元素的X（水平，左端）位置
+function pageX(elem) {
+    // 查看我们是否位于根元素
+    return elem.offsetParent ?
+        // 如果我们能继续得到上一个元素，增加当前的偏移量并继续向上递归
+         elem.offsetLeft + pageX(elem.offsetParent) :
+        // 否则，获取当前的偏移量
+         elem.offsetLeft;
+}
+// 获取元素的y（垂直，顶端）位置
+function pageY(elem) {
+    // 查看我们是否位于根元素
+    return elem.offsetParent ?
+        // 如果我们能继续得到上一个元素，增加当前的偏移量并继续向上递归
+        elem.offsetTop + pageY(elem.offsetParent) :
+        // 否则，获取当前的偏移量
+        elem.offsetTop;
+}
+
+
+//-----确定元素相对于父亲的位置的两个函数--------
+function parentX(elem) {
+    // 如果offsetParent 是元素的父亲，那么提前退出
+    // 否则，我们需要找到元素和元素的父亲相对于整个页面位置，并计算他们之间的差
+    return elem.parentNode == elem.offsetParent ? elem.offsetLeft : pageX(elem) - pageX(elem.parentNode);
+}
+// 获取元素相对于父亲的垂直位置
+function parentY(elem) {
+    // 如果offsetParent 是元素的父亲，那么提前退出
+    // 否则，我们需要找到元素和元素的父亲相对于整个页面位置，并计算他们之间的差
+    return elem.parentNode == elem.offsetParent ? elem.offsetTop : pageY(elem) - pageY(elem.parentNode);
+}
+
+
+//------ 获取元素的CSS位置的辅助函数---------------------
+  // 查找元素的左端位置
+function posX(elem) {
+    // 获取最终样式并得到数值
+    return parseInt( getStyle(elem,"left"));
+}
+ // 查找元素的顶端位置
+function posY(elem) {
+    // 获取最终样式并得到数值
+    return parseInt( getStyle(elem,"top"));
+}
+
+/* ----------  设置位置 -------------- */
+
+//------ 设置元素x和y位置（与当前位置无关）的一对函数
+//设置元素水平位置的函数
+function setX(elem,pos) {
+    // 使用像素单位设置CSS的'left'属性
+    elem.style.left = pos + "px";
+}
+//设置元素垂直位置的函数
+function setY(elem,pos) {
+    // 使用像素单位设置CSS的'top'属性
+    elem.style.top = pos + "px";
+}
+
+//------ 调整元素相对于当前位置的距离的一对函数 ----------------
+// 在元素的水平位置上增加像素距离的函数
+function addX(elem,pos) {
+    // 获取当前水平位置，然后增加偏移量
+    setX(elem,posX(elem) +pos);
+}
+// 在元素的垂直位置上增加像素距离的函数
+function addY(elem,pos) {
+    // 获取当前垂直位置，然后增加偏移量
+    setY(elem,posY(elem) +pos);
+}
+
+/** ------------- 元素的尺寸 ------------------- */
+
+/* ---------获取元素当前的高度和宽度 -------- */
+// 获取元素的真实高度（使用CSS最终样式值）
+function getHeight(elem) {
+    // 获取CSS的最终值并解析出可用的数值
+    return parseInt( getStyle(elem,'height'))
+}
+// 获取元素的真实宽度（使用CSS最终样式值）
+function getWidth(elem) {
+    // 获取CSS的最终值并解析出可用的数值
+    return parseInt( getStyle(elem,'width'))
+}
+
+/* ----- 即使元素隐藏，亦能分别获取它潜在的完整高度和宽度的两个参数  -------*/
+// 查找元素完整的、可能的高度
+function fullHeight( elem ) {
+    // 如果元素时显示的，那么使用 offsetHeight 就能得到高度，如果没有offsetHeight
+    // 则使用getHeight()
+    if ( getStyle( elem, 'display' ) != 'none' ){
+        return elem.offsetHeight || getHeight( elem );
+    }else{
+        // 否则，我们必须处理display 为none 的元素，
+        // 并且重置它的css属性以获取更精确的读数
+        var old = resetCSS( elem, {
+            display: '',
+            visibility: 'hidden',
+            position: 'absolute'
+        });
+
+        // 使用clientHeight 找出元素的完整高度，
+        // 如果还不生效，则使用getHeight()函数
+        var h = elem.clientHeight || getHeight( elem );
+
+        // 最后，恢复CSS的原有属性
+        restoreCSS( elem, old );
+
+        // 并返回元素的完整高度
+        return h;
+    }
+
+}
+
+// Find the full, possible, width of an element (not the actual,
+// current, width)
+function fullWidth( elem ) {
+    // If the element is being displayed, then offsetWidth
+    // should do the trick, barring that, getWidth() will work
+    if ( getStyle( elem, 'display' ) != 'none' )
+        return elem.offsetWidth || getWidth( elem );
+
+    // Otherwise, we have to deal with an element with a display
+    // of none, so we need to reset its CSS properties to get a more
+    // accurate reading
+    var old = resetCSS( elem, {
+        display: '',
+        visibility: 'hidden',
+        position: 'absolute'
+    });
+
+    // Figure out what the full width of the element is, using clientWidth
+    // and if that doesn't work, use getWidth
+    var w = elem.clientWidth || getWidth( elem );
+
+    // Finally, restore the CSS properties back to what they were
+    restoreCSS( elem, old );
+
+    // and return the full width of the element
+    return w;
+}
+
+// 设置CSS 一组属性的函数，它可以恢复到原有设置
+function resetCSS( elem, prop ) {
+    var old = {};
+
+    // 遍历每一个函数
+    for ( var i in prop ) {
+        // 记录旧的属性值
+        old[ i ] = elem.style[ i ];
+
+        // 并设置新的属性值
+        elem.style[ i ] = prop[i];
+    }
+
+    // 返回已经变化的值的集合，预留给restoreCSS 函数使用
+    return old;
+}
+
+// 恢复CSS原有属性，防止resetCSS 函数副作用的函数
+function restoreCSS( elem, prop ) {
+    // 重置所以属性，恢复他们的原有值
+    for ( var i in prop )
+        elem.style[ i ] = prop[ i ];
+}
