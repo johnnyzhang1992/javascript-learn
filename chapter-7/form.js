e1e8d912eb0dec8e1c08cf78a445c825d7bb5fa7
johnnyzhang <1113638674@qq.com>
2017-04-10 12:22:02 +0800

    chapter-7 form-显示错误以及可用性的提升

--- chapter-7/form.js
+++ chapter-7/form.js
@@ -35,6 +35,15 @@ function getInputByName(name) {
     // 返回匹配的字段集合
     return results;
 }
+/** ------ 模式匹配 ---------------*/
+
+/*-----电子邮件 ------*/
+//--------------- 检查指定的input元素是否包含电子邮件地址
+// 检查input元素是否符合email地址要求的通用函数
+function checkEmail( elem ) {
+    // 确保输入的内容是正确的email地址
+    return !elem.value || /^[a-z0-9_+.-]+\@([a-z0-9-]+\.)+[a-z0-9]{2,4}$/i.test(elem.value);
+}
 // 等待文档完成加载
 window.onload = function () {
     // 获得表单并监听提交事件
@@ -58,5 +67,191 @@ window.onload = function () {
             console.warn("Required field is empty - "+ "please provide your name.");
             return false
         }
+       // 获取需要检查的input元素
+        var elem2 = document.getElementById('email');
+        // 检查这个字段是否正确
+        if(!checkEmail(elem2)){
+            console.warn("Field is not an email address.");
+            return false;
+        }
     };
 };
+// Error Message
+var errMsg = {
+    // Checks for when a specified field is required
+    required: {
+        msg: "This field is required.",
+        test: function(obj,load) {
+            // Make sure that something was not entered and that this
+            // isn't on page load (showing 'field required' messages
+            // would be annoying on page load)
+            return obj.value || load || obj.value == obj.defaultValue;
+        }
+    },
+
+    // Makes sure that the field s a valid email address
+    email: {
+        msg: "Not a valid email address.",
+        test: function(obj) {
+            // Make sure that something was entered and that it looks like
+            // an email address
+            return !obj.value ||
+                /^[a-z0-9_+.-]+\@([a-z0-9-]+\.)+[a-z0-9]{2,4}$/i.test( obj.value );
+        }
+    },
+
+    // Makes sure the field is a phone number and
+    // auto-formats the number if it is one
+    phone: {
+        msg: "Not a valid phone number.",
+        test: function(obj) {
+            // Check to see if we have something that looks like
+            // a valid phone number
+            var m = /(\d{3}).*(\d{3}).*(\d{4})/.exec( obj.value );
+
+            // If it is, seemingly, valid - force it into the specific
+            // format that we desire: (123) 456-7890
+            if ( m ) obj.value = "(" + m[1] + ") " + m[2] + "-" + m[3];
+
+            return !obj.value || m;
+        }
+    },
+
+    // Makes sure that the field is a valid MM/DD/YYYY date
+    date: {
+        msg: "Not a valid date.",
+        test: function(obj) {
+            // Make sure that something is entered, and that it
+            // looks like a valid MM/DD/YYYY date
+            return !obj.value || /^\d{2}\/\d{2}\/\d{2,4}$/.test(obj.value);
+        }
+    },
+
+    // Makes sure that the field is a valid URL
+    url: {
+        msg: "Not a valid URL.",
+        test: function(obj) {
+            // Make sure that some text was entered, and that it's
+            // not the default http:// text
+            return !obj.value || obj.value == 'http://' ||
+                // Make sure that it looks like a valid URL
+                /^https?:\/\/([a-z0-9-]+\.)+[a-z0-9]{2,4}.*$/.test( obj.value );
+        }
+    }
+};
+function validateForm( form, load ) {
+    var valid = true;
+
+    // Go through all the field elements in form
+    // form.elements is an array of all fields in a form
+    for ( var i = 0; i < form.elements.length; i++ ) {
+
+        // Hide any error messages, if they're being shown
+        hideErrors( form.elements[i] );
+
+        // Check to see if the field contains valid contents, or not
+        if ( ! validateField( form.elements[i], load ) )
+            valid = false;
+
+    }
+
+    // Return false if a field does not have valid contents
+    // true if all fields are valid
+    return valid;
+}
+
+// Validate a single field's contents
+function validateField( elem, load ) {
+    var errors = [];
+
+    // Go through all the possible validation techniques
+    for ( var name in errMsg ) {
+        // See if the field has the class specified by the error type
+        var re = new RegExp("(^|\\s)" + name + "(\\s|$)");
+
+        // Check to see if  the element has the class and that it passes the
+        // validatino test
+        if ( re.test( elem.className ) && !errMsg[name].test( elem, load ) )
+        // If it fails the validation, add the error message to list
+            errors.push( errMsg[name].msg );
+    }
+
+    // Show the error messages, if they exist
+    if ( errors.length )
+        showErrors( elem, errors );
+
+    // Return false if the field fails any of the validation routines
+    return errors.length > 0;
+}
+// Hide any validation error messages that are currently shown
+function hideErrors( elem ) {
+    // Find the next element after the current field
+    var next = elem.nextSibling;
+
+    // If the next element is a ul and has a class of errors
+    if ( next && next.nodeName == "UL" && next.className == "errors" )
+    // Remove it (which is our means of  'hiding')
+        elem.parenttNode.removeChild( next );
+}
+
+// Show a set of errors messages for a specific field within a form
+function showErrors( elem, errors ) {
+    // Find the next element after the field
+    var next = elem.nextSibling;
+
+    // If the field isn't one of our special error-holders.
+    if ( next && ( next.nodeName != "UL" || next.className != "errors" ) ) {
+        // We need to make one instead
+        next = document.createElement( "ul" );
+        next.className = "errors";
+
+        // and then insert into the correct place in the DOM
+        elem.paretNode.insertBefore( next, elem.nextSibling );
+    }
+
+    // Now that we have a reference to the error holder UL
+    // We then loop through all the error messages
+    for ( var i = 0; i < errors.length; i++ ) {
+        // Create a new li wrapper for each
+        var li = document.createElement( "li" );
+        li.innerHTML = errors[i];
+
+        // and insert it into the DOM
+        next.appendChild( li );
+    }
+}
+function watchForm( form ) {
+    // Watch the form for submission
+    addEvent( form, 'submit',  function(){
+
+        // make sure that the form's contents validate correctly
+        return validateForm( form );
+
+    });
+}
+function watchFields( form ) {
+    // Go through all the field elements in form
+    for ( var i = 0; i < form.elements.length; i++ ) {
+
+        // and attach a 'blur' event handler (which watches for a user
+        // to lose focus of an input element)
+        addEvent( form.elements[i], 'blur',  function(){
+            // Once the focus has been lost, re-validate the field
+            return validateField( this );
+        });
+
+    }
+}
+addEvent( window, "load", function() {
+    // Find all the forms on the page
+    var forms = document.getElementsByTagName("form");
+
+    // Go through all the forms on the page
+    for ( var i = 0; i < forms.length; i++ ) {
+
+        // Validate each of the forms, being sure to set the 'load' argument to
+        // true, which stops certain, unnecessary, errors from appearing
+        validateForm( forms[i], true );
+
+    }
+});
