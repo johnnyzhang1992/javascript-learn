69a568d8503cf1d2e6a5facfe7945f83d7b97b3d
johnnyzhang <1113638674@qq.com>
2017-05-03 18:50:02 +0800

    表达式-循环

--- chapter-1/表达式和运算符/statement.js
+++ chapter-1/表达式和运算符/statement.js
@@ -16,3 +16,19 @@ function convert(x) {
     }
 }
 // for/in
+
+// 标签语句
+var token = null;
+mainloop : while(token != null){
+    token = '';
+    continue mainloop; //跳转到下一次循环
+    token = 'aaaa';//不被执行
+}
+//try catch
+try{
+    var n = Number(prompt('请输入一个整数',''));
+    var f = factorial(n);
+    console.log(f);
+}catch(ex){
+    alert(ex);//告诉用户发生了什么错误
+}
