54832defca2b55e95faf242bd35e1a594c415bc7
johnnyzhang <1113638674@qq.com>
2017-05-25 11:58:57 +0800

    函数

--- chapter-1/函数/function.js
+++ chapter-1/函数/function.js
@@ -16,12 +16,17 @@
 function f1() {
 
 }
+
+// 命名函数表达式
+// f1 对函数外部不可访问
+(function f1() {});
+
 // 函数定义表达式
 var f2 = function () {
 
 };
 
-// 定义匿名函数
+// 匿名函数表达式
 
 (function () {
     console.log('aaaa')
