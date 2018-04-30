d5695e31724a747d205342727310eb8817a6dff6
johnnyzhang <1113638674@qq.com>
2018-04-30 23:40:32 +0800

    es5 数组

--- chapter-1/数组/array.js
+++ chapter-1/数组/array.js
@@ -85,7 +85,45 @@ console.log(arr7.sort(compare1));//[ 11, 10, 8, 5, 5, 3, 2, 1 ]
 
 console.log(arr7.reverse(compare));// [ 1, 2, 3, 5, 5, 8, 10, 11 ]
 console.log(arr7.reverse(compare1));// [ 11, 10, 8, 5, 5, 3, 2, 1 ]
-
+// 降序
+function compare2(val1,val2) {
+    return val2-val1;
+}
+// val2 > val1 return true,大的在前面
+console.log('---compare2---');
+console.log(arr7.sort(compare2));
 // pop 取出最后一条数据
 // shift 取出第一条数据
 // push 末尾添加数据
+
+//concat 联合
+
+let color = ['red'];
+let color2 = color.concat('green',['blue','black']);
+console.log(color2);//[ 'red', 'green', 'blue', 'black' ]
+
+// slice 剪切
+
+let color3 = [ 'red', 'green', 'blue', 'black','purple'];
+console.log(color3.slice(1));// 1-5 [ 'green', 'blue', 'black', 'purple' ]
+console.log(color3.slice(1,4));// [ 'green', 'blue', 'black' ]
+console.log(color3.slice(-2,-1));// 3,4 [ 'black' ]
+
+// splice 粘接 ，删除/插入/替换
+console.log('-------splice------');
+// 返回删除项
+// 要删除的第一项的位置，要删除的位置
+console.log(color3.splice(0,2)); //[ 'red', 'green' ]
+console.log(color3);// [ 'blue', 'black', 'purple' ]
+// 返回删除项
+//  起始位置，要删除的项数，要插入的项
+let color4 = color3.splice(1,0,'yellow','orange');
+console.log(color3);// [ 'blue', 'yellow', 'orange', 'black', 'purple' ]
+console.log(color4);// []
+// 分析：从1开始，删除0项，添加两项
+
+// 替换
+let color5 = color3.splice(2,1,'white','gray');
+console.log(color5);// [ 'orange' ]
+console.log(color3);// [ 'blue', 'yellow', 'white', 'gray', 'black', 'purple' ]
+//分析： 从2开始，删除一项，添加两项，即其中替换一项，新增一项
