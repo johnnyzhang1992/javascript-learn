a78b7ebb4569a98ff6a1972cd070614221d3718c
johnnyzhang <1113638674@qq.com>
2018-05-15 22:57:41 +0800

    es5 基本包装类型 String

---
+++ chapter-1/基本包装类型/string 类型.js
@@ -0,0 +1,25 @@
+/**
+ * Created by PhpStorm.
+ * User: johnnyZhang
+ * Date: 2018/5/15
+ * Time: 22:51
+ */
+
+let stringObject = new String('hello world');
+
+console.log(stringObject); // [String: 'hello world']
+console.log(typeof stringObject); // object
+
+// 方法
+
+// match()
+// search()
+// replace()
+
+// indexOf()
+// lastIndexOf()
+
+// toLowerCase()
+// toUpperCase()
+
+// split() 字符串转数组
