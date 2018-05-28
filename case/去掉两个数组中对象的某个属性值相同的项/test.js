e6b1f5d114fb98db8c425c199c3f3f28c7a12acc
johnnyzhang <1113638674@qq.com>
2018-05-28 10:12:49 +0800

    列子，去掉两个数组中相同属性相同值的元素

---
+++ case/去掉两个数组中对象的某个属性值相同的项/test.js
@@ -0,0 +1,66 @@
+/**
+ * Author johnnyZhang
+ * Site johnnyzhang.cn
+ * CreateTime 2018/5/26.
+ */
+let _aa = [{
+    'id': 1,
+    'name': '123'
+}, {
+    'id': 4,
+    'name': '143',
+    "code": '2'
+}, {
+    'id': 6,
+    'name': '153'
+}, {
+    'id': 5,
+    'name': '1573'
+}];
+let _bb = [{
+    'id': 1,
+    'name': '123'
+}, {
+    'id': 4,
+    'name': '143',
+    "code": '2'
+},{
+    'id':10,
+    'name': 'sssss'
+}];
+// 获取数组中同一属性值相同的元素，并返回组合数组
+function twoArraySameData(arr1, arr2,propertyName) {
+    let arr_ids  = [];
+    arr1.concat(arr2).map(function (item) {
+        arr_ids.push(item[propertyName])
+    });
+    return arr_ids;
+}
+// 数组相同元素留下，然后去重
+function trimSameData(array){
+    // 排序
+    let sort_arr = array.sort(function (val1,val2) {
+        return val1 > val2
+    });
+    let new_arr = [];
+    // 去重
+    for(let i=0;i<sort_arr.length;i++){
+        if(sort_arr[i] == sort_arr[i+1]){
+            new_arr.push(sort_arr[i])
+        }
+    }
+    return new_arr;
+}
+
+let deleteSameIdObject = function (arr1,arr2,propertyName) {
+    let same_ids = trimSameData(twoArraySameData(arr1,arr2,propertyName));
+    let concatArr = arr1.concat(arr2);
+    let resultArr = [];
+    concatArr.map(function (item) {
+        if(same_ids.indexOf(item[propertyName]) == -1){
+            resultArr.push(item);
+        }
+    });
+    return resultArr;
+};
+console.log(deleteSameIdObject(_aa,_bb,'id'));
---
+++ case/去掉两个数组中对象的某个属性值相同的项/test1.js
@@ -0,0 +1,52 @@
+/**
+ * Author johnnyZhang
+ * Site johnnyzhang.cn
+ * CreateTime 2018/5/26.
+ */
+/**
+ * Author johnnyZhang
+ * Site johnnyzhang.cn
+ * CreateTime 2018/5/26.
+ */
+let _aa = [{
+    'id': 1,
+    'name': '123'
+}, {
+    'id': 4,
+    'name': '143',
+    "code": '2'
+}, {
+    'id': 6,
+    'name': '153'
+}, {
+    'id': 5,
+    'name': '1573'
+}];
+let _bb = [{
+    'id': 1,
+    'name': '123'
+}, {
+    'id': 4,
+    'name': '143',
+    "code": '2'
+},{
+    'id':10,
+    'name': 'sssss'
+}];
+function TwoArrDeleteSameDate(arr1,arr2,propertyName) {
+    // 先合并
+    let concatArr = arr1.concat(arr2);
+    // 排序
+    concatArr.sort(function (item1,item2) {
+        return item1[propertyName] > item2[propertyName]
+    });
+    // 遍历过程中，相同项删除
+    for(let i=concatArr.length-1;i>0;i--){
+        if(concatArr[i][propertyName] == concatArr[i-1][propertyName]){
+            concatArr.splice(i,1);
+            concatArr.splice(i-1,1)
+        }
+    }
+    return concatArr;
+}
+console.log(TwoArrDeleteSameDate(_aa,_bb,'id'));
