dc0c53426c66c0db2b1ee615ea879c1295764588
johnnyzhang <1113638674@qq.com>
2018-08-02 12:48:41 +0800

    ES6 Object

--- ES6/object.js
+++ ES6/object.js
@@ -8,9 +8,10 @@
  */
 
 // 方法一
-obj.foo = true;
+
+// obj.foo = true;
 // 方法二
-obj['a' + 'bc'] = 123;
+// obj['a' + 'bc'] = 123;
 
 // ES6 允许字面量定义对象时，用方法二（表达式）作为对象的属性名，即把表达式放在方括号内。
 let propKey = 'foo';
@@ -21,6 +22,7 @@ let obj = {
 
 /*
 Object.assign()
+对象克隆
 */
 // Object.assign方法用于对象的合并，将源对象（source）的所有可枚举属性，复制到目标对象（target）。
 
@@ -30,5 +32,11 @@ var source1 = { b: 2 };
 var source2 = { c: 3 };
 
 Object.assign(target, source1, source2);
-target ;// {a:1, b:2, c:3}
+console.log(target) ;// {a:1, b:2, c:3}
 // Object.assign方法的第一个参数是目标对象，后面的参数都是源对象。
+
+// 如果想要保持继承链，可以采用下面的代码。
+function clone(origin) {
+    let originProto = Object.getPrototypeOf(origin);
+    return Object.assign(Object.create(originProto), origin);
+}
