d60e2c12a84658a04054e6c82dee61d60cabbe57
johnnyzhang1992 <zq19920306@gmail.com>
2018-09-04 18:19:32 +0800

    ES6 Reflect 映射

--- ES6/reflect.js
+++ ES6/reflect.js
@@ -13,13 +13,17 @@
 // 比如，Object.defineProperty(obj, name, desc)在无法定义属性时，
 // 会抛出一个错误，而Reflect.defineProperty(obj, name, desc)则会返回false。
 //
-// // 老写法
+let target = {};
+let property = 'name';
+let attributes = {'s':'sss'};
+// 老写法
 try {
     Object.defineProperty(target, property, attributes);
     // success
 } catch (e) {
     // failure
 }
+console.log(target);
 //
 // // 新写法
 if (Reflect.defineProperty(target, property, attributes)) {
@@ -27,16 +31,33 @@ if (Reflect.defineProperty(target, property, attributes)) {
 } else {
     // failure
 }
+console.log(target);
 // （3） 让Object操作都变成函数行为。某些Object操作是命令式，
 // 比如name in obj和delete obj[name]，而Reflect.has(obj, name)
 // 和Reflect.deleteProperty(obj, name)让它们变成了函数行为。
 //
 // // 老写法
-'assign' in Object // true
+console.log('assign' in Object );// true
 //
 // // 新写法
-Reflect.has(Object, 'assign'); // true
+console.log(Reflect.has(Object, 'assign')); // true
 // （4）Reflect对象的方法与Proxy对象的方法一一对应，只要是Proxy对象的方法，
 // 就能在Reflect对象上找到对应的方法。这就让Proxy对象可以方便地调用对应的Reflect方法，
 // 完成默认行为，作为修改行为的基础。
 // 也就是说，不管Proxy怎么修改默认行为，你总可以在Reflect上获取默认行为。
+
+let proxy = new Proxy(target, {
+    set: function(target, name, value, receiver) {
+        let success = Reflect.set(target,name, value, receiver);
+        if (success) {
+            console.log('property ' + name + ' on ' + target + ' set to ' + value);
+        }
+        return success;
+    }
+});
+proxy.u_name = 'johnny'; //property u_name on [object Object] set to johnny
+// proxy.age = '222';
+// proxy.score = 60;
+console.log(proxy); // { u_name: 'johnny' }
+// 上面代码中，Proxy方法拦截target对象的属性赋值行为。
+// 它采用Reflect.set方法将值赋值给对象的属性，确保完成原有的行为，然后再部署额外的功能。
